b96da5c1862890f3b1f09fb907ef1b768e2685a2
choiyukyung <121738855+choiyukyung@users.noreply.github.com>
2024-12-07 13:13:26 +0900

    Implement Home Screen (not finished)

---
+++ app/src/main/java/com/example/myfridge/feature/home/AddNewItemScreen.kt
@@ -0,0 +1,9 @@
+package com.example.myfridge.feature.home
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AddNewItemScreen(navController: NavController) {
+
+}
