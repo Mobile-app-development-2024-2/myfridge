9a0f8c2acb23533d454964d4d77128d0c734f8ff
Kredsya <do901328@gmail.com>
2024-12-09 19:35:33 +0900

    Add prototype single page

--- app/src/main/java/com/example/myfridge/feature/food/FoodListScreen.kt
+++ app/src/main/java/com/example/myfridge/feature/food/FoodListScreen.kt
@@ -1,6 +1,9 @@
 package com.example.myfridge.feature.food
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,10 +16,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -34,12 +41,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -66,6 +78,8 @@ fun FoodListScreen(navController: NavController) {
 
     val foodList by viewModel.foodList.collectAsState(emptyList())
     var searchWhat by remember { mutableStateOf("") }
+    var selectedFood by remember { mutableStateOf<Food?>(null) }
+    var isDetailVisible by remember { mutableStateOf(false) }
 
     val filteredList = if (searchWhat.isEmpty()) {
         foodList
@@ -142,6 +156,8 @@ fun FoodListScreen(navController: NavController) {
                             .height(80.dp)
                             .clickable {
                                 // 단일 페이지
+                                selectedFood = item
+                                isDetailVisible = true
                             },
                     ) {
                         Row(
@@ -192,6 +208,165 @@ fun FoodListScreen(navController: NavController) {
                                 .padding(1.dp)
                         )
 
+                    }
+                }
+            }
+        }
+
+        if (isDetailVisible && selectedFood != null) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable { isDetailVisible = false },
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                Column(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(600.dp)
+                        .background(Color.White)
+                        .padding(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onClick = { isDetailVisible = false}) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.button_delete),
+                                contentDescription = "Close"
+                            )
+                        }
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_food),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .padding(vertical = 5.dp, horizontal = 10.dp)
+                                .align(Alignment.CenterVertically)
+                        )
+                    }
+                    Text(
+                        text = selectedFood!!.name + "\n가성비 알아보기",
+                        style = MaterialTheme.typography.titleLarge,
+                        textAlign = TextAlign.End,
+                        modifier = Modifier.fillMaxWidth(),
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.dotted_line2),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(10.dp),
+                        colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply {
+                            setToSaturation(0f)
+                        })
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                            .border(
+                                width = 4.dp,
+                                color = Color(0xFF01D1C4),
+                                shape = RoundedCornerShape(16.dp)
+                            )
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(Color.White),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "0원",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(top = 12.dp, bottom = 4.dp)
+                        )
+                        Text(
+                            text = "저렴하게 구매했습니다",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(top = 4.dp, bottom = 12.dp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.dotted_line2),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(10.dp),
+                        colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply {
+                            setToSaturation(0f)
+                        })
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "내가 구매한 가격",
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.End,
+                        modifier = Modifier.fillMaxWidth().padding(4.dp),
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "${selectedFood!!.price}원",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(color = MintWhite)
+                            .padding(8.dp),
+                        textAlign = TextAlign.End
+                    )
+                    Text(
+                        text = "vs",
+                        style = MaterialTheme.typography.titleLarge,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(
+                        text = "가성비 가격",
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.End,
+                        modifier = Modifier.fillMaxWidth().padding(4.dp),
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "0원",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(color = MintWhite)
+                            .padding(8.dp),
+                        textAlign = TextAlign.End
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            // sortedFoodList = sortedFoodList.filter { it != selectedFood }
+                            // todo : viewModel을 통해 삭제
+                            isDetailVisible = false
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.White,
+                            contentColor = Color.Red
+                        ),
+                        border = BorderStroke(2.dp, Color.Red)
+                    ) {
+                        Text(text = "항목 삭제", color = Color.Red)
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(
+                        onClick = { /** todo */ },
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF01D1C4),
+                            contentColor = Color.White
+                        ),
+                        shape = RoundedCornerShape(4.dp)
+                    ) {
+                        Text(text = "장보기로 이동")
                     }
                 }
             }
